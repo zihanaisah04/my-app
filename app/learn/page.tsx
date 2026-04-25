9fed9925dfa10c00fe0ec1b1dd28ef08038ae8c3
jiun <azihanaisah@gmail.com>
2026-04-25 20:08:02 +0800

    next.js

---
+++ app/learn/page.tsx
@@ -0,0 +1,14 @@
+export default function learn(){
+    return(
+        <div>
+<div>
+ <div>
+    <img></img>
+    <p>&#9827; design</p>
+ </div>
+    <h1>Figma</h1>
+    <p>8 hours ago</p>
+</div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            halo aku zihan
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
@@ -52,11 +52,11 @@ export default function Home() {
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="http://localhost:3000/Blog" 
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            zihan
           </a>
         </div>
       </main>
